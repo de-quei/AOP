c7e7a7a0aec60844a5d138dd0335c8c0d4fbccf0
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-04-18 12:20:39 +0900

    Initial commit

---
+++ src/main/java/kr/hs/Study/Beans/TestBean1.java
@@ -0,0 +1,10 @@
+package kr.hs.Study.Beans;
+
+public class TestBean1 {
+    public void method1() {
+        System.out.println("TestBean1의 method1 호출");
+    }
+    public void method2() {
+        System.out.println("TestBean1의 method2 호출");
+    }
+}
